85f76eb849fb2d02503cb5164d163eaf91d53995
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2023-05-22 09:04:49 +0600

    Add "Contains Duplicate" solution in python

---
+++ src/leetcode/interview/easy/Contains Duplicate/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+        
+        for n in nums:
+            if n in dict:
+                return True
+            dict[n] = True
+            
+        return False
