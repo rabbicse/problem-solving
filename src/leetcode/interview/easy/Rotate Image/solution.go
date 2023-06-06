0e922ccf85b6af4f1099ebb7daa41a387b322f7e
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2023-06-06 09:02:47 +0600

    Add "Rotate Image" solution in Go

---
+++ src/leetcode/interview/easy/Rotate Image/solution.go
@@ -0,0 +1,17 @@
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+
+	// Transposing the matrix
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	// Reversing each row of the matrix
+	for i := 0; i < n; i++ {
+		for j := 0; j < n / 2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
