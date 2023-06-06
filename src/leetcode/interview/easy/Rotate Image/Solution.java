8e6cbca9890b4f14234e6825555208e0f6e6aec9
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2023-06-06 09:00:10 +0600

    Add "Rotate Image" solution in java

---
+++ src/leetcode/interview/easy/Rotate Image/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // Transposing the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Reversing each row of the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
