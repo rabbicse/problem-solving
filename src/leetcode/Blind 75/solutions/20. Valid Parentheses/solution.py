08d22beb5e166415cf32295d2206906dbb371030
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2024-04-23 15:30:58 +0600

    Create solution.py

---
+++ src/leetcode/Blind 75/solutions/20. Valid Parentheses/solution.py
@@ -0,0 +1,21 @@
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hash_map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        stack = []
+        for ch in s:
+            if stack and ch in hash_map:
+                item = stack.pop()
+                if item != hash_map[ch]:
+                    return False
+            else:
+                stack.append(ch)
+        return not stack
