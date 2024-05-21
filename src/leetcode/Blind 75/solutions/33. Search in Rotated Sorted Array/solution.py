9851e85eae6f1e586a89f34168bde94ac448fc14
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2024-05-21 09:25:11 +0600

    Create solution.py

---
+++ src/leetcode/Blind 75/solutions/33. Search in Rotated Sorted Array/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # find target
+            if nums[mid] == target:
+                return mid
+            
+            # subarray on mid's left is sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # subarray on mid's right is sorted
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+        
