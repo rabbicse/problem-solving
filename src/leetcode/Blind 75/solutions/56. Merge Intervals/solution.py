d22b060075c09da2cbc4c8279ea203f7cbed0e1e
MD. MEHEDI HASAN <rabbi.se@gmail.com>
2024-05-23 09:41:16 +0600

    Create solution.py

---
+++ src/leetcode/Blind 75/solutions/56. Merge Intervals/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        result = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+
+        return result
